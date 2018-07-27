450325f8250fda699d6e30717ecbab020baf01f2
rohit <rohit.2.gupta@atos.net>
2018-07-28 00:56:23 +0100

    routing and gaurd invalid url activated routing

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,25 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list.component';
 import { ConverttToSpacesPipe } from './shared/Convert-To-Spaces.Pipe';
 import { StarComponent } from './shared/star.component';
-import { ProviderAst } from '../../node_modules/@angular/compiler';
 import { ProductService } from './product/product-list.service';
 import { HttpClientModule } from '../../node_modules/@angular/common/http';
+import { ProductDetailComponent } from './product/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailGuard } from './product/product-detail.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConverttToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot ([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id',canActivate:[ProductDetailGuard], component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+
+    ] )
   ],
   bootstrap: [AppComponent],
   providers:[ProductService]
